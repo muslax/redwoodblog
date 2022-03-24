214e3c429508b291fbf995b3b9385f374305f2b4
muslax <muslax@gmail.com>
2022-03-24 15:21:32 +0700

    Sampai auth

---
+++ web/src/components/ArticleCell/ArticleCell.js
@@ -0,0 +1,31 @@
+import Article from 'src/components/Article'
+
+// export const QUERY = gql`
+//   query FindArticleQuery($id: Int!) {
+//     article: article(id: $id) {
+//       id
+//     }
+//   }
+// `
+export const QUERY = gql`
+  query ArticleQuery($id: Int!) {
+    article: post(id: $id) {
+      id
+      title
+      body
+      createdAt
+    }
+  }
+`
+
+export const Loading = () => <div>Loading...</div>
+
+export const Empty = () => <div>Empty</div>
+
+export const Failure = ({ error }) => (
+  <div style={{ color: 'red' }}>Error: {error.message}</div>
+)
+
+export const Success = ({ article }) => {
+  return <Article article={article} />
+}
